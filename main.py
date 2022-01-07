2f50b4cfcc2489982673fbb2d25e6a33e4ed7462
ntut-xuan <t109590031@ntut.org.tw>
2022-01-07 14:55:03 +0800

    'update(2022/01/07)'

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import requests
 import json
 from io import BytesIO
 from flask import *
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 app = Flask(__name__)
 
@@ -22,11 +24,20 @@ def getLogin():
     index_html = open("./login.html", "r", encoding="utf-8")
     return index_html.read()
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def getRegister():
-    index_html = open("./register.html", "r", encoding="utf-8")
-    return index_html.read()
+    if request.method == "GET":
+        index_html = open("./register.html", "r", encoding="utf-8")
+        return index_html.read()
+
+@app.route("/platform", methods=["GET"])
+def getPlatform():
+    if request.method == "GET":
+        index_html = open("./platform.html", "r", encoding="utf-8")
+        return index_html.read()
 
 if __name__ == "__main__":
+    cred = credentials.ApplicationDefault()
+    firebase_admin.initialize_app(cred, {'projectId': project_id,})
     app.debug = True
     app.run(host="0.0.0.0", port=80)
